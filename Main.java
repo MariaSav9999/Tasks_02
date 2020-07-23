b74d7ecd187e2d1e8b173ef37d654a57b1247d84
MariaSav9999 <68650418+MariaSav9999@users.noreply.github.com>
2020-07-24 01:08:17 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package by.htp.les02.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			double a;
+			double b;
+			
+			a = 7.3;
+			b = 2.9;
+			
+			if (a < b) {
+				System.out.println ("Число а меньше");
+			}  else{
+					if(a > b){
+						System.out.println ("Число b больше или равно");
+					}	else {
+							System.out.println("числа a и b равны");
+						}
+			}
+			
+			}
+	}	
